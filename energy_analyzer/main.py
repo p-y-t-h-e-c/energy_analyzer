45f33b4b2df0b46c15d96f81835389484a7d7114
pkoper <pkoper@jaguarlandrover.com>
2024-02-15 15:02:40 +0000

    refactor(dagster): trasfered rest of main.py code to run in dagster dag

--- energy_analyzer/main.py
+++ energy_analyzer/main.py
@@ -64,9 +64,10 @@ def get_electricity_rates_data() -> pd.DataFrame:
     return data_to_add_to_db
 
 
-# @task(name="Add Octopus electricity rates data to database")
 @asset(name="Add_Octopus_Electricity_Rates_Data_to_Database")
-def add_electricity_rates_data_to_db(Get_Octopus_Electricity_Rates_Data) -> None:
+def add_electricity_rates_data_to_db(
+    Get_Octopus_Electricity_Rates_Data: pd.DataFrame,
+) -> None:
     """Add Octopus electricity rates data to database.
 
     Args:
@@ -78,7 +79,6 @@ def add_electricity_rates_data_to_db(Get_Octopus_Electricity_Rates_Data) -> None
     )
 
 
-# @task(name="Get Octopus gas rates data")
 @asset(name="Get_Octopus_Gas_Rates_Data", deps=[add_electricity_rates_data_to_db])
 def get_gas_rates_data() -> pd.DataFrame:
     """Get Octopus standard unit rates data."""
@@ -104,9 +104,8 @@ def get_gas_rates_data() -> pd.DataFrame:
     return data_to_add_to_db
 
 
-# @task(name="Add Octopus gas rates data to database")
-@asset
-def add_gas_rates_data_to_db(Get_Octopus_Gas_Rates_Data) -> None:
+@asset(name="Add_Octopus_Gas_Rates_Data_to_Database")
+def add_gas_rates_data_to_db(Get_Octopus_Gas_Rates_Data: pd.DataFrame) -> None:
     """Add Octopus gas rates data to database.
 
     Args:
@@ -117,8 +116,10 @@ def add_gas_rates_data_to_db(Get_Octopus_Gas_Rates_Data) -> None:
     )
 
 
-# @task(name="Get Octopus electricity daily consumption data")
-@asset(deps=[add_gas_rates_data_to_db])
+@asset(
+    name="Get_Octopus_Electricity_Daily_Consumption_Data",
+    deps=[add_gas_rates_data_to_db],
+)
 def get_electricity_consumption_data() -> pd.DataFrame:
     """Get Octopus electricity consumption data."""
     data_extractor = DataExtractor()
@@ -146,22 +147,25 @@ def get_electricity_consumption_data() -> pd.DataFrame:
     return data_to_add_to_db
 
 
-# @task(name="Add Octopus electricity consumption data to database")
-@asset
-def add_electricity_consumption_data_to_db(get_electricity_consumption_data) -> None:
+@asset(name="Add_Octopus_Electricity_Consumption_Data_to_Database")
+def add_electricity_consumption_data_to_db(
+    Get_Octopus_Electricity_Daily_Consumption_Data: pd.DataFrame,
+) -> None:
     """Add Octopus electricity consumption data to database.
 
     Args:
         data: electricity consumption data in the form of DataFrame
     """
     DB_CONNECTOR.add_data_to_db(
-        get_electricity_consumption_data,
+        Get_Octopus_Electricity_Daily_Consumption_Data,
         table_name=ElectricityConsumptionTable.__tablename__,
     )
 
 
-# @task(name="Get Octopus gas daily consumption data")
-@asset
+@asset(
+    name="Get_Octopus_Gas_Daily_Consumption_Data",
+    deps=[add_electricity_consumption_data_to_db],
+)
 def get_gas_consumption_data() -> pd.DataFrame:
     """Get Octopus gas consumption data."""
     data_extractor = DataExtractor()
@@ -187,21 +191,25 @@ def get_gas_consumption_data() -> pd.DataFrame:
     return data_to_add_to_db
 
 
-# @task(name="Add Octopus gas consumption data to database")
-@asset
-def add_gas_consumption_data_to_db(get_gas_consumption_data: pd.DataFrame) -> None:
+@asset(name="Add_Octopus_Gas_Consumption_Data_to_Database")
+def add_gas_consumption_data_to_db(
+    Get_Octopus_Gas_Daily_Consumption_Data: pd.DataFrame,
+) -> None:
     """Add Octopus gas consumption data to database.
 
     Args:
         data: gas daily consumption data in the form of DataFrame
     """
     DB_CONNECTOR.add_data_to_db(
-        get_gas_consumption_data, table_name=GasConsumptionTable.__tablename__
+        Get_Octopus_Gas_Daily_Consumption_Data,
+        table_name=GasConsumptionTable.__tablename__,
     )
 
 
-# @task(name="Get Octopus electricity weekly consumption data")
-@asset
+@asset(
+    name="Get_Octopus_Electricity_Weekly_Consumption_Data",
+    deps=[add_gas_consumption_data_to_db],
+)
 def get_electricity_weekly_consumption_data() -> pd.DataFrame:
     """Get Octopus electricity weekly consumption data."""
     data_extractor = DataExtractor()
@@ -226,10 +234,9 @@ def get_electricity_weekly_consumption_data() -> pd.DataFrame:
     return electricity_consumption_weekly_formatted
 
 
-# @task(name="Add Octopus electricity weekly consumption data to database")
-@asset
+@asset(name="Add_Octopus_Electricity_Weekly_Consumption_Data_to_Database")
 def add_electricity_weekly_consumption_data_to_db(
-    get_electricity_weekly_consumption_data: pd.DataFrame,
+    Get_Octopus_Electricity_Weekly_Consumption_Data: pd.DataFrame,
 ) -> None:
     """Add Octopus electricity weekly consumption data to database.
 
@@ -237,14 +244,16 @@ def add_electricity_weekly_consumption_data_to_db(
         data: electricity weekly consumption data in the form of DataFrame
     """
     DB_CONNECTOR.add_data_to_db(
-        get_electricity_weekly_consumption_data,
+        Get_Octopus_Electricity_Weekly_Consumption_Data,
         table_name=ElectricityWeeklyConsumptionTable2024.__tablename__,
         if_exists="replace",
     )
 
 
-# @task(name="Get Octopus gas weekly consumption data")
-@asset
+@asset(
+    name="Get_Octopus_Gas_Weekly_Consumption_Data",
+    deps=[add_electricity_weekly_consumption_data_to_db],
+)
 def get_gas_weekly_consumption_data() -> pd.DataFrame:
     """Get Octopus gas weekly consumption data."""
     data_extractor = DataExtractor()
@@ -267,10 +276,9 @@ def get_gas_weekly_consumption_data() -> pd.DataFrame:
     return gas_consumption_weekly_formatted
 
 
-# @task(name="Add Octopus gas weekly consumption data to database")
-@asset
+@asset(name="Add_Octopus_Gas_Weekly_Consumption_Data_to_Database")
 def add_gas_weekly_consumption_data_to_db(
-    get_gas_weekly_consumption_data: pd.DataFrame,
+    Get_Octopus_Gas_Weekly_Consumption_Data: pd.DataFrame,
 ) -> None:
     """Add Octopus gas weekly consumption data to database.
 
@@ -278,71 +286,7 @@ def add_gas_weekly_consumption_data_to_db(
         data: gas weekly consumption data in the form of DataFrame
     """
     DB_CONNECTOR.add_data_to_db(
-        get_gas_weekly_consumption_data,
+        Get_Octopus_Gas_Weekly_Consumption_Data,
         table_name=GasWeeklyConsumptionTable2024.__tablename__,
         if_exists="replace",
     )
-
-
-# # @flow(name="Process Octopus Data")
-# def process_octopus_data():
-#     """Process Octopus Data from API to database."""
-#     LOGGER.info("Extracting electricity rates data.")
-#     electricity_rates_data = get_electricity_rates_data()
-
-#     LOGGER.info("Adding electricity rates data to DB.")
-#     electricity_rates_data_to_db = add_electricity_rates_data_to_db(
-#         electricity_rates_data
-#     )
-
-#     time.sleep(1)
-
-#     LOGGER.info("Extracting gas rates data.")
-#     gas_rates_data = get_gas_rates_data()
-
-#     LOGGER.info("Adding gas rates data to DB.")
-#     gas_rates_data_to_db = add_gas_rates_data_to_db(gas_rates_data)
-
-#     time.sleep(1)
-
-#     LOGGER.info("Extracting electricity consumption data.")
-#     electricity_consumption_data = get_electricity_consumption_data()
-
-#     LOGGER.info("Adding electricity consumption data to DB.")
-#     electricity_consumption_data_to_db = add_electricity_consumption_data_to_db(
-#         electricity_consumption_data
-#     )
-
-#     time.sleep(1)
-
-#     LOGGER.info("Extracting gas consumption data.")
-#     gas_consumption_data = get_gas_consumption_data()
-
-#     LOGGER.info("Adding gas consumption data to DB.")
-#     gas_consumption_data_to_db = add_gas_consumption_data_to_db(gas_consumption_data)
-
-#     time.sleep(1)
-
-#     LOGGER.info("Extracting electricity weekly consumption data.")
-#     electricity_weekly_consumption_data = get_electricity_weekly_consumption_data()
-
-#     LOGGER.info("Adding electricity weekly consumption data to DB.")
-#     electricity_consumption_weekly_data_to_db = (
-#         add_electricity_weekly_consumption_data_to_db(
-#             electricity_weekly_consumption_data
-#         )
-#     )
-
-#     time.sleep(1)
-
-#     LOGGER.info("Extracting gas weekly consumption data.")
-#     gas_weekly_consumption_data = get_gas_weekly_consumption_data()
-
-#     LOGGER.info("Adding gas weekly consumption data to DB.")
-#     gas_consumption_weekly_data_to_db = add_gas_weekly_consumption_data_to_db(
-#         gas_weekly_consumption_data
-#     )
-
-
-# if __name__ == "__main__":
-#     process_octopus_data()
